51238fd4e6dd52346e107531ae69acc30bca6d28
Jorar Academic Portal <131459549+jorarportal@users.noreply.github.com>
2023-04-26 10:52:07 +0200

    Update inc.php

--- inc.php
+++ inc.php
@@ -6,10 +6,10 @@ ob_start();
 require_once 'func.php';
 
 //db connection
-$host = '127.0.0.1';
-$db   = 'short_url';
-$user = 'tutor_user';
-$pass = 'Malembe123!';
+$host = '';
+$db   = '';
+$user = '';
+$pass = '';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $opt = [
@@ -22,9 +22,9 @@ $db = new PDO($dsn, $user, $pass, $opt);
 $environment = "dev";
 
 if($environment=="prod"){    
-    $site_url = "hbt.lol";
+    $site_url = "your_production_url e.g https://prod.url";
 }elseif($environment=="dev"){
-    $site_url = "http://localhost/shorturl";
+    $site_url = "your_localhost_url";
 }
 
  ?>
